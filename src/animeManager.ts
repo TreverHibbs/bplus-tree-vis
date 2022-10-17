623979c4e1cc1a5505549cb68c0d3b18628ce53b
TreverHibbs <treverhibbs@gmail.com>
2022-10-17 11:14:09 -0700

    init animation manager class

---
+++ src/animeManager.ts
@@ -0,0 +1,29 @@
+export class AnimeManager {
+    /**
+     * Creates a svg representation of a bplus tree node
+     */
+    public createNode() {
+        return 1
+    }
+
+    /**
+     * Deletes a svg representation of a bplus tree node
+     */
+    public deleteNode() {
+        return 1
+    }
+
+    /**
+     * translates a bplus tree node from point A to B
+     */
+    public translateNode() {
+        return 1
+    }
+
+    /**
+     * creates a edge from one node to another
+     */
+    public createEdge() {
+        return 1
+    }
+}
