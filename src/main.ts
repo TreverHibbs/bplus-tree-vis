3aeb39dedf40b8e3d7ad123b4e437c5ed7a53a14
TreverHibbs <treverhibbs@gmail.com>
2022-10-25 19:19:25 -0700

    init insert sudo code

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,10 @@ window.onload = () => {
   const algoVisualizer = new AlgoVisualizer(4)
   console.debug("insertion return", algoVisualizer.insert(42))
   console.debug("find return", algoVisualizer.find(42))
+
+  const sudoCodeContainer = document.querySelector("#sudo-code")
+  const insertSudoCode = document.querySelector("#insert-sudo-code")
+  if (sudoCodeContainer?.innerHTML && insertSudoCode?.innerHTML) {
+    sudoCodeContainer.innerHTML = insertSudoCode?.innerHTML
+  }
 }
