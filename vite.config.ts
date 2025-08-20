264f3be5d3d6331602df7a022e2fcf98a33eb66d
TreverHibbs <treverhibbs@gmail.com>
2025-08-20 09:55:01 -0700

    start ghpages fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-    server: {
-        port: 3002,
-    },
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+    if (command === 'serve') {
+        return {
+            server: {
+                port: 3002
+            }
+        }
+    } else {
+        return {
+            base: '/bplus-tree-vis/',
+            build: {
+                outDir: './',
+                assetsInlineLimit: () => true
+            }
+        }
+    }
 });
